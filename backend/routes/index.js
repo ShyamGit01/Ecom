58c65f744ebc9062a5b62e40ceb0b56c78b40d00
ShyamGit01 <shyamasundars43@gmail.com>
2021-07-27 20:10:02 +0530

    initial

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/bulk', function(req, res, next) {
+  try{
+    var data = req.body;
+    console.log(data);
+    var final_count = Object.keys(data).length;
+    var count = 0;
+    while(count <= final_count){
+      console.log(data[count].product_Id);
+      count++;
+    }
+   
+  } catch(error){
+    res.status(400).json({
+      status: 0,
+      message: "bulk initiating error"
+    });
+  }
 });
 
 module.exports = router;
