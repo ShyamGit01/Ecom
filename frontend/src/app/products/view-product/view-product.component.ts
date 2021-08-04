c6dca1621eb76d6805c219122b7a4cb839336473
ShyamGit01 <shyamasundars43@gmail.com>
2021-08-04 12:28:50 +0530

    view product id get

--- frontend/src/app/products/view-product/view-product.component.ts
+++ frontend/src/app/products/view-product/view-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-product',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewProductComponent implements OnInit {
 
-  constructor() { }
+  productID = 0; 
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((data:any) => {
+      this.productID = data;
+      console.log(data);
+    })
   }
 
 }
